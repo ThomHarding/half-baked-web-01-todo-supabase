42fe315af82aa1aeefbc81773f49c280eff0bccb
THOMAS HARDING <hardingt604@gmail.com>
2022-03-28 13:20:02 -0600

    first try at fetch-utils functins

--- fetch-utils.js
+++ fetch-utils.js
@@ -6,25 +6,47 @@ const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 export async function createTodo(todo) {
     // create a single incomplete todo with the correct 'todo' property for this user in supabase
-
+    const response = await client
+        .from ('todos')
+        .insert({
+            todo: todo,
+            complete: false,
+            user_id: client.auth.user().id,
+        })
+        .single();
     return checkError(response);
 }
 
 export async function deleteAllTodos() {
     // delete all todos for this user in supabase
-
+    const response = await client
+        .from ('todos')
+        .delete()
+        .match({ user_id: client.auth.user().id });
     return checkError(response);
 }
 
 export async function getTodos() {
     // get all todos for this user from supabase
-
+    const response = await client
+        .from ('todos')
+        .select()
+        .match({ user_id: client.auth.user().id });
     return checkError(response);
 }
 
 export async function completeTodo(id) {
     // find the and update (set complete to true), the todo that matches the correct id
-
+    const response = await client
+        .from ('todos')
+        .update({
+            complete: true
+        })
+        .match({
+            user_id: client.auth.user().id,
+            id: id,
+        })
+        .single();
     return checkError(response);
 }
 
@@ -38,13 +60,13 @@ export function checkAuth() {
     if (!user) location.replace('../');
 }
 
-export function redirectIfLoggedIn() {
+export async function redirectIfLoggedIn() {
     if (await getUser()) {
         location.replace('./todos');
     }
 }
 
-export function signupUser(email, password) {
+export async function signupUser(email, password) {
     const response = await client.auth.signUp({ email, password });
 
     return response.user;
